86547628410ce44a1a42aedd1dfb5498145c8dac
ZeinetdinovMarsel <marsel.zeinetdinov@gmail.com>
2023-05-21 18:58:06 +0400

    Getting Started переделан на 7 лабу, 8 в разработке

---
+++ 7 лаба/mysite/polls/forms.py
@@ -0,0 +1,64 @@
+from django import forms
+from .models import Item, Cashier, Store, Check, Sale
+
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = '__all__'
+        labels = {
+            'name': 'Название товара',
+        }
+
+
+class CashierForm(forms.ModelForm):
+    class Meta:
+        model = Cashier
+        fields = ['name', 'phone_number']
+        labels = {
+            'name': 'Имя кассира',
+            'phone_number': 'Номер телефона кассира',
+        }
+
+
+class StoreForm(forms.ModelForm):
+    class Meta:
+        model = Store
+        fields = ['name', 'address']
+        labels = {
+            'name': 'Название магазина',
+            'address': 'Адрес магазина',
+        }
+
+
+class CheckForm(forms.ModelForm):
+    class Meta:
+        model = Check
+        fields = ['cashier', 'date_time', 'total_sum', 'store']
+        labels = {
+            'cashier': 'Кассир',
+            'date_time': 'Дата и время',
+            'total_sum': 'Общая сумма',
+            'store': 'Магазин',
+        }
+        widgets = {
+            'date_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'total_sum': forms.NumberInput(attrs={
+                'step': 10,
+                'min': 0,
+                'max': 999999,
+                'decimal_places': 2,
+                'style': 'width: 100px;'
+            })
+        }
+
+
+class SaleForm(forms.ModelForm):
+    class Meta:
+        model = Sale
+        fields = ['check_id', 'item', 'quantity']
+        labels = {
+            'check_id': 'ID чека',
+            'item': 'Товар',
+            'quantity': 'Количество',
+        }
